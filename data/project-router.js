708ca50f9f7c9c6f80e60b3d07be510278d73f61
GUL-Bao <BaoPham92@yahoo.com>
2019-11-01 12:31:30 -0400

    Completed projects CRUD

--- data/project-router.js
+++ data/project-router.js
@@ -2,20 +2,52 @@ const express = require('express');
 const router = express.Router();
 const projects = require('./helpers/projectModel');
 
-router.get('/', (req, res) => {
-
+router.get('/', async (req, res) => {
+    await projects.get()
+        .then(projectList => res.status(200).json(projectList))
+        .catch(err => res.status(500).json({ errorMessage: "Not able to return projects!" }))
+    res.end();
 });
 
-router.get('/:id', (req, res) => {
+router.get('/:id', async (req, res) => {
+    const id = req.params.id;
 
+    !!id === true &&
+        await projects.get(id)
+            .then(projectById => res.status(200).json(projectById))
+            .catch(err => res.status(500).json({ errorMessage: "GIVE ME FLOWER FIRST, THEN YOU GET THE PROJECT." }))
+    res.end();
 });
 
-router.delete('/:id', (req, res) => {
+router.post('/', async (req, res) => {
+    const project = req.body;
 
+    !!project === true &&
+        await projects.insert(project)
+            .then(project => res.status(201).json(project))
+            .catch(err => res.status(500).json({ errorMessage: "NOOOOOOOOO POSTING FOR YOU BOIII" }))
+    red.end();
 });
 
-router.put('/:id', (req, res) => {
+router.delete('/:id', async (req, res) => {
+    const id = req.params.id;
 
+    !!id === true &&
+        await projects.remove(id)
+            .then(project => res.status(200).json({ success: `successfully delete this object, if it appears ${project}` }))
+            .catch(err => res.status(500).json({ errorMessage: "GIVE ME FLOWER, OR NO DELETE FOR YOU!" }))
+    res.end();
+});
+
+router.put('/:id', async (req, res) => {
+    const id = req.params.id;
+    const project = req.body;
+
+    !!project === true && !!project === true &&
+        await projects.update(id, project)
+            .then(updatedProject => res.status(201).json(updatedProject))
+            .catch(err => res.status(500).json({ errorMessage: "FLOWER, OR NO UPDATE FOR YOU." }))
+    res.end();
 });
 
 module.exports = router;
